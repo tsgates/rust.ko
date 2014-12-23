d23b995ad12a992b550030b88a46dc0229774b36
panicbit <panicbit@users.noreply.github.com>
2014-12-23 02:52:49 +0100

    Correct mutability

--- main.rs
+++ main.rs
@@ -7,11 +7,11 @@ extern crate core;
 use core::mem::transmute;
 
 extern {
-    pub fn printk(fmt: *mut u8);
+    pub fn printk(fmt: *const u8);
 }
 
 unsafe fn print(s: &str) {
-    let (ptr, _): (*mut u8, uint) = transmute(s);
+    let (ptr, _): (*const u8, uint) = transmute(s);
     printk(ptr);
 }
 
