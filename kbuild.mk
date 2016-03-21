efb156c2909118926a09a26a82dd361637ac456b
Theo Belaire <theo.belaire@gmail.com>
2016-03-21 13:48:11 -0400

    Improved makefile

--- kbuild.mk
+++ kbuild.mk
@@ -19,10 +19,18 @@ src := ${BASE_DIR}
 C_FILES    := $(foreach filepath,${C_FILES}   ,${BASE_DIR}/$(filepath))
 RUST_FILES := $(foreach filepath,${RUST_FILES},${BASE_DIR}/$(filepath))
 
+CARGO_OUT_DIR := ${BASE_DIR}/target/${UTS_MACHINE}-unknown-none-gnu
+
+ifeq (${RELEASE},1)
+	CARGO_OUT_DIR :=${CARGO_OUT_DIR}/release
+else
+	CARGO_OUT_DIR :=${CARGO_OUT_DIR}/debug
+endif
+
 # Build rule for Rust target object
 # Note: UTS_MACHINE is the architecture of the target kernel, Rust compilation will  fail  unless  a
 #       target file (such as "x86_64-unknown-none-gnu.json") was created for the architecture of the
 #       kernel you're trying to compile this module for.
 $(obj)/${rust-target}: ${RUST_FILES}
 	cd "${BASE_DIR}" && $(CARGO) build --verbose ${CARGOFLAGS} --target="${UTS_MACHINE}-unknown-none-gnu" -- ${RCFLAGS}
-	cp "${BASE_DIR}"/target/x86_64-unknown-none-gnu/debug/${rust-target} $(obj)
+	cp "${CARGO_OUT_DIR}/${rust-target}" $(obj)
