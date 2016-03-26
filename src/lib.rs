9c54ccf9cb33b9fbd2fc89baa74aff59fdf3b175
Alexander Schlarb <alexander@xmine128.tk>
2016-03-26 23:26:52 +0100

    Drop shipped macros, functions and types in favor of interfaces exposed by linux-std

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
-#![feature(core_str_ext)]
 #![feature(custom_attribute, lang_items)]
 #![no_std]
 
 #[macro_use]
-mod macros;
+extern crate linux_std as std;
 
-mod raw;
-
-// Defines various symbols that need to be around.
+// Defines various language items that need to be around
 mod lang;
-
-pub mod types;
 
 #[no_mangle]
 pub fn rust_main() {
