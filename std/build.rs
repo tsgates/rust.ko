063518818c847b38e235ca6e39078d51e3a5716d
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2016-03-31 08:39:31 +0800

    Fix clang flag error https://github.com/tsgates/rust.ko/issues/19

--- std/build.rs
+++ std/build.rs
@@ -18,10 +18,11 @@ const CLANG_HEADER_REQUIRED: [&'static str; 3] = [
 ];
 
 /// List of parameters not ever to pass to the clang parser of rust-bindgen
-const CLANG_ARGS_BLACKLIST: [&'static str; 9] = [
+const CLANG_ARGS_BLACKLIST: [&'static str; 10] = [
 	"-mno-80387", "-mno-fp-ret-in-387", "-mskip-rax-setup", "-maccumulate-outgoing-args",
 	"-mpreferred-stack-boundary=3", "-mfentry",
-	"-fno-var-tracking-assignments", "-fconserve-stack", "-DCC_HAVE_ASM_GOTO"
+	"-fno-var-tracking-assignments", "-fconserve-stack", "-DCC_HAVE_ASM_GOTO",
+	"-fno-delete-null-pointer-checks"
 ];
 
 
